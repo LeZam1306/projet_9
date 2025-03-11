b84a0d7120f70f6ec38d37284817042dc8398af3
LeZam <adamzamouricontact@gmail.com>
2025-03-11 15:31:10 +0100

    use of "last" in Context

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,8 +13,7 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const { data } = useData()
-  const last = data?.events[data.events.length - 1];
+  const { last } = useData();
   return <>
     <header>
       <Menu />
